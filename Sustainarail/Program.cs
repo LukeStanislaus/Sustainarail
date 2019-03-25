08066ec3da98c929431cfdb8d9e158affe0e6018
Luke Stanislaus <luke@stanislaus.co.uk>
2019-03-25 10:31:42 +0000

    mn

---
+++ Sustainarail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sustainarail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
